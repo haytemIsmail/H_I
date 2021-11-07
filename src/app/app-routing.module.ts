363c258789aff31fb5bd35c97b741d860efd04fe
Haytem <i.haytem@inofrdata.net>
2021-11-07 18:05:04 +0100

    inserimento_bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ const routes: Routes = [
 	{ path: "home", component: HomeComponent },
 	{ path: "login", component: LoginComponent },
 	{ path: "crypto", component: CryptoComponent },
-	{ path: "", redirectTo: "/", pathMatch: "full" },
 	{ path: '**', component: LoginComponent }
 ];
 
